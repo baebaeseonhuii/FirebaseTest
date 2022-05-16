7ebd8e8dbe9b61f0703cc20a67a114cacf3ad7dc
baebaeseonhuii <48781193+baebaeseonhuii@users.noreply.github.com>
2022-05-16 17:56:12 +0900

    create, fetch, update, delete

--- FirebaseTest/ViewController.swift
+++ FirebaseTest/ViewController.swift
@@ -11,13 +11,21 @@ import Firebase
 class ViewController: UIViewController {
     
     @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var numOfCustomers: UILabel!
     let db = Database.database().reference()
+    
+    var customers: [Customer] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabel()
         saveBasicTypes()
-        saveCustomers()
+        //saveCustomers()
+        fetchCustomers()
+        
+        //update, delete
+        //updateBasicTypes()
+        //deleteBasicTypes()
     }
     
     func updateLabel() {
@@ -31,8 +39,39 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    
+    @IBAction func createCustomer(_ sender: Any) {
+        saveCustomers()
+    }
+    
+    
+    @IBAction func fetchCustomer(_ sender: Any) {
+        fetchCustomers()
+    }
+    
+    func updateCustomers() {
+        guard customers.isEmpty == false else { return }
+        customers[0].name = "Hee"
+        
+        let dictionary = customers.map { $0.toDictionary }
+        db.updateChildValues(["customers": dictionary])
+    }
+    
+    @IBAction func updateCustomer(_ sender: Any) {
+        updateCustomers()
+    }
+    
+    func deleteCustomers() {
+        
+    }
+    
+    @IBAction func deleteCustomer(_ sender: Any) {
+        deleteCustomers()
+    }
 }
 
+// MARK: Add Data
 extension ViewController {
     func saveBasicTypes() {
         // Firebase child ("key").setValue(Value)
@@ -61,9 +100,52 @@ extension ViewController {
     }
 }
 
-struct Customer {
+// MARK: Read(Fetch) Data
+extension ViewController {
+    func fetchCustomers() {
+        db.child("customers").observeSingleEvent(of: .value) { snapshot in
+            print("--> \(snapshot.value)")
+            
+            do {
+                let data = try JSONSerialization.data(withJSONObject: snapshot.value, options: [])
+                
+                let decoder = JSONDecoder()
+                let customers: [Customer] = try decoder.decode([Customer].self, from: data)
+                self.customers = customers
+                DispatchQueue.main.async {
+                    self.numOfCustomers.text = "# Of Customers: \(customers.count)"
+                }
+                
+            } catch let error {
+                print("--> error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+extension ViewController {
+    func updateBasicTypes() {
+        
+        db.updateChildValues(["int": 6])
+        db.updateChildValues(["double": 5.4])
+        db.updateChildValues(["str": "string chnged"])
+            
+//        db.child("int").setValue(3)
+//        db.child("double").setValue(3.5)
+//        db.child("str").setValue("string value - 여러분 안녕")
+    }
+    
+    func deleteBasicTypes() {
+        db.child("int").removeValue()
+        db.child("double").removeValue()
+        db.child("str").removeValue()
+    }
+}
+
+
+struct Customer: Codable {
     let id: String
-    let name: String
+    var name: String
     let books: [Book]
     
     var toDictionary: [String: Any] {
@@ -74,7 +156,7 @@ struct Customer {
     static var id: Int = 0
 }
 
-struct Book {
+struct Book: Codable {
     let title: String
     let author: String
     
